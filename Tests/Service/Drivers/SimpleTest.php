7fa3ac5b061b6b97f3986f8ba355c4fcefb186cd
Adrià Cidre <adria.cidre@wuaki.tv>
2013-02-19 19:17:25 +0100

    Empty tests

---
+++ Tests/Service/Drivers/SimpleTest.php
@@ -0,0 +1,6 @@
+<?php
+
+class SimpleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ Tests/Service/Drivers/WgetTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodify\DownloaderBundle\Tests\Controller;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class WgetTest extends \PHPUnit_Framework_TestCase
+{
+
+}
